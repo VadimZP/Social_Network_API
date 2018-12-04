fe4004d5bb20148f6d6d804fcf3e833760790bb6
Vadim <wesborlandzp@gmail.com>
2018-12-04 13:36:48 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 $config['settings']['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-$config['db']['host']   = 'localhost';
-$config['db']['user']   = 'root';
-$config['db']['pass']   = '';
-$config['db']['dbname'] = 'socialnetwork';
+$config['db']['host']   = 'eu-cdbr-west-02.cleardb.net';
+$config['db']['user']   = 'b2f20b50597a89';
+$config['db']['pass']   = '2fa2cf0f';
+$config['db']['dbname'] = 'heroku_e01fe9332aa758f';
