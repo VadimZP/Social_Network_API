194a00cf183090a5ac4f398ef558d7db9c714435
Vadim <wesborlandzp@gmail.com>
2018-12-04 13:52:47 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ $config['settings']['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
 $config['db']['host']   = 'eu-cdbr-west-02.cleardb.net';
-$config['db']['user']   = 'b2f20b50597a89';
+$config['db']['user']   = 'bb8adb9d2f6467';
 $config['db']['pass']   = '2fa2cf0f';
 $config['db']['dbname'] = 'heroku_e01fe9332aa758f';
