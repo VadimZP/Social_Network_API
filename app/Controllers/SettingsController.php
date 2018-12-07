ba3a26bfcfed442fc633f25a89c7059dcafcc303
Vadim <wesborlandzp@gmail.com>
2018-12-07 16:00:31 +0200

    S3Bucket config update

--- app/Controllers/SettingsController.php
+++ app/Controllers/SettingsController.php
@@ -8,6 +8,31 @@ use Aws\S3\S3Client;
 class SettingsController extends Controller {
 
     public function uploadAvatar($request, $response) {
+      /*   $files = $request->getUploadedFiles();
+        $userId = $request->getParam('userId');
+        $avatar = $files['file'];
+
+        if (!file_exists("$userId/")) {
+            mkdir("$userId/");
+        }
+
+        $fileSearch = "avatar";
+        $files = glob("D:/Soft/xampp/htdocs/socialnetwork/server/public/$userId/*" . $fileSearch . "*");
+
+        if(count($files) > 0) unlink($files[0]);
+
+        if($avatar->getError() === UPLOAD_ERR_OK) {
+            $name = explode(".", $avatar->getClientFilename());
+            $name[0] = "avatar" . uniqid();
+            $name = join(".", $name);
+            $whitelist = array('127.0.0.1','::1');
+            
+            if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+                $avatar->moveTo("D:/Soft/xampp/htdocs/socialnetwork/server/public/$userId/$name");
+            } else {
+                $avatar->moveTo("D:/Soft/xampp/htdocs/socialnetwork/server/public/$userId/$name");
+            }
+        } */
         $s3 = S3Client::factory(
             array(
                 'credentials' => array(
@@ -22,13 +47,13 @@ class SettingsController extends Controller {
         $keyName = 'test_example/' . basename($_FILES["file"]['tmp_name']);
         $pathInS3 = 'https://s3.eu-central-1.amazonaws.com/social-network-zp/' . $keyName;
 
-        $file = $_FILES["file"];
+        $file = $_FILES["file"]['tmp_name'];
 
 		$s3->putObject(
 			array(
 				'Bucket'=> 'social-network-zp',
 				'Key' =>  $keyName,
-				'SourceFile' => $file,
+				'SourceFile' => 'C:/Users/iceman/Desktop/Temp',
 				'StorageClass' => 'REDUCED_REDUNDANCY'
 			)
 		);
