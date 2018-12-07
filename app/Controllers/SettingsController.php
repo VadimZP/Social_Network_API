e4210667353ca7f770ec6a228fe44cf2c93c5002
Vadim <wesborlandzp@gmail.com>
2018-12-07 16:07:32 +0200

    Update SettingsController.php

--- app/Controllers/SettingsController.php
+++ app/Controllers/SettingsController.php
@@ -53,7 +53,7 @@ class SettingsController extends Controller {
 			array(
 				'Bucket'=> 'social-network-zp',
 				'Key' =>  $keyName,
-				'SourceFile' => 'C:/Users/iceman/Desktop/Temp/white-girls-black-white.jpg',
+				'SourceFile' => 'C:\Users\iceman\Desktop\Temp\white-girls-black-white.jpg',
 				'StorageClass' => 'REDUCED_REDUNDANCY'
 			)
 		);
